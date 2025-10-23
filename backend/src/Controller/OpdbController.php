f9c604f479d22bd25b71e0e2781dfd5b29f05696
Rhokapa <pierrick.pobelle@gmail.com>
2025-10-23 19:34:30 +0200

    upgrade and mail sending

--- backend/src/Controller/OpdbController.php
+++ backend/src/Controller/OpdbController.php
@@ -77,6 +77,7 @@ return $this->render('reset_password/email.html.twig',[
     #[Route('/api/public/search/game/{opdbid}', methods: ['GET'])]
     public function index(string $opdbid, OpdbService $opdbService): JsonResponse
     {
+        var_dump('plop');
         $machines = $opdbService->searchMachine($opdbid);
         return $this->json($machines);
     }
@@ -84,6 +85,8 @@ return $this->render('reset_password/email.html.twig',[
     #[Route('/api/public/search/game', methods: ['GET'])]
     public function test2(Request $request, OpdbService $opdbService): JsonResponse
     {
+        var_dump('ploup');
+
         $machines = $opdbService->searchMachineGroups($request->query->get('query'));
         return $this->json(array_values($machines));
     }
